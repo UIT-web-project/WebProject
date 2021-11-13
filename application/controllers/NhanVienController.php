86de692636fa1f1feb3254da2ee248fcf52b858b
Nguyen Huu Thang <cambayst@gmail.com>
2021-11-13 10:34:23 +0700

    create controller & loadview

---
+++ application/controllers/NhanVienController.php
@@ -0,0 +1,18 @@
+<?php 
+
+	
+	defined('BASEPATH') OR exit('No direct script access allowed');
+	
+	class NhanVienController extends CI_Controller {
+	
+		public function index()
+		{
+			$this->load->view('NhanVienView');
+			
+		}
+	
+	}
+	
+	/* End of file NhanVienController.php */
+	
+?>
